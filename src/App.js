7e257b641dccc93b99a80168e46e9360f196915f
IndrajitSir <indrajitmandal779@gmail.com>
2025-01-27 11:06:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <h2 style={{textAlign: "center",color: "red"}}>wlcome to my college</h2>
+      <p className="para">our first website</p>
+    </div>
+  );
+}
+
+export default App;
