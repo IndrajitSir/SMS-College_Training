ce2a7fc1c7c7285fb2b8aeded8bfc6b051e4cf5e
IndrajitSir <indrajitmandal779@gmail.com>
2025-01-28 11:33:12 +0530

    understand useState hook in depth

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home(){
+    const [data, setData] = useState(0);
+    const handleClick=()=>{
+        setData(data+1)
+    }
     return(
         <div>
             <center>
+            <h1>
             Home Page
+            </h1>
+            <button onClick={handleClick}>Click here</button>
+            <h2>{`value=${data}`}</h2>
             </center>
         </div>
     )
